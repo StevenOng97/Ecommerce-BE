4f9682a5c3fb51e6332db386d1bfd8a26b4ec0ca
Nguyen <giaanguyen221197@gmail.com>
2022-03-14 19:52:52 +0700

    update

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,22 +1,42 @@
 const multer = require('multer');
 const { GridFsStorage } = require('multer-gridfs-storage');
 
-const storage = new GridFsStorage({
-  url: process.env.MONGODB_URL,
-  options: { useNewUrlParser: true, useUnifiedTopology: true },
-  file: (req, file) => {
-    const match = ['image.png', 'image/jpeg', 'image.jpg', 'image/webp'];
+const path = require('path');
 
-    if (match.indexOf(file.mimetype) === -1) {
-      const filename = `${Date.now()}-${file.originalname}`;
-      return filename;
+// Multer config
+module.exports = multer({
+  storage: multer.diskStorage({}),
+  fileFilter: (req, file, cb) => {
+    let ext = path.extname(file.originalname);
+    if (
+      ext !== '.jpg' &&
+      ext !== '.jpeg' &&
+      ext !== '.png' &&
+      ext !== '.webp'
+    ) {
+      cb(new Error('File type is not supported'), false);
+      return;
     }
-
-    return {
-      bucketName: 'photos',
-      filename: `${Date.now()}-${file.originalname}`,
-    };
+    cb(null, true);
   },
 });
 
-module.exports = multer({ storage });
+// const storage = new GridFsStorage({
+//   url: process.env.MONGODB_URL,
+//   options: { useNewUrlParser: true, useUnifiedTopology: true },
+//   file: (req, file) => {
+//     const match = ['image.png', 'image/jpeg', 'image.jpg', 'image/webp'];
+
+//     if (match.indexOf(file.mimetype) === -1) {
+//       const filename = `${Date.now()}-${file.originalname}`;
+//       return filename;
+//     }
+
+//     return {
+//       bucketName: 'photos',
+//       filename: `${Date.now()}-${file.originalname}`,
+//     };
+//   },
+// });
+
+// module.exports = multer({ storage });
--- src/routes/product.js
+++ src/routes/product.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Product = require('../models/Product');
 const upload = require('../middleware/upload');
 const auth = require('../middleware/auth');
+const { cloudinary } = require('../../cloudinary');
 
 router.get('/products', async (req, res) => {
   const qNew = req.query.new;
@@ -30,7 +31,7 @@ router.get('/products', async (req, res) => {
         .limit(limit * 1)
         .skip((page - 1) * limit);
     }
-    
+
     res.status(200).json(products);
   } catch (err) {
     res.status(500).json(err);
@@ -48,23 +49,34 @@ router.get('/find/:id', async (req, res) => {
 
 router.post('/products', auth, upload.array('file', 3), async (req, res) => {
   if (req.files.length < 3) return res.send('Please select a file.');
-  const imgUrls = [];
-  req.files.map(({ filename }) => {
-    imgUrls.push(`ecommerce-be.vercel.app/file/${filename}`);
+
+  const promises = req.files.map(async (file) => {
+    return await cloudinary.v2.uploader.upload(
+      file.path,
+      { folder: 'ecommerce' },
+      function (error, result) {
+        if (error) {
+          return res.status(500).send("Getting Error while uploading Images");
+        }
+        return result;
+      }
+    );
   });
+
+  const uploadedFiles = await Promise.all(promises);
+  const mappedFiles = uploadedFiles.map((file) => file.secure_url);
 
   const product = new Product({
     title: req.body.title,
     price: req.body.price,
     isNew: req.body.isNew || true,
-    images: imgUrls,
+    images: mappedFiles,
     categories: req.body.categories,
     desc: req.body.desc,
     sale: req.body?.sale,
     priceAfterSale: req.body?.priceAfterSale,
   });
 
-  console.log(product);
   try {
     await product.save();
     res.status(201).send(product);
@@ -73,30 +85,30 @@ router.post('/products', auth, upload.array('file', 3), async (req, res) => {
   }
 });
 
-// router.put('/products/:id', auth, upload.array('file', 3), async (req, res) => {
-//   if (req.files.length < 3) return res.send('Please select a file.');
-//   const imgUrls = [];
-//   req.files.map(({ filename }) => {
-//     imgUrls.push(filename);
-//   });
+router.put('/products/:id', auth, upload.array('file', 3), async (req, res) => {
+  if (req.files.length < 3) return res.send('Please select a file.');
+  const imgUrls = [];
+  req.files.map(({ filename }) => {
+    imgUrls.push(filename);
+  });
 
-//   const product = new Product({
-//     name: req.body.name,
-//     price: req.body.price,
-//     isNew: true,
-//     images: imgUrls,
-//     category: req.body.category,
-//   });
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.price,
+    isNew: true,
+    images: imgUrls,
+    category: req.body.category,
+  });
 
-//   try {
-//     await product.save();
-//     res.status(201).send(product);
-//   } catch (err) {
-//     throw new Error('Error', err);
-//   }
-// });
+  try {
+    await product.save();
+    res.status(201).send(product);
+  } catch (err) {
+    throw new Error('Error', err);
+  }
+});
 
-router.delete('/:id', auth, async (req, res) => {
+router.delete('/products/:id', auth, async (req, res) => {
   try {
     await Product.findByIdAndDelete(req.params.id);
     res.status(200).json('Product has been deleted...');
